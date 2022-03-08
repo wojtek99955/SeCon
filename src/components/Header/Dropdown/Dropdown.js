1cd4e435d40710793faf1b845c8ba232fb3c1f2e
Wojtek Książek <wojtek99955@gmail.com>
2022-03-08 08:50:56 +0100

    dropdown onmouseenter transition

--- src/components/Header/Dropdown/Dropdown.js
+++ src/components/Header/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
@@ -7,10 +7,15 @@ const DropdownContainer = styled.div`
     position: absolute;
     top:100%;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.4);
+    overflow: hidden;
+    transition: height 0.5s ease-in-out;
+    height: ${({ $dropdown, height }) => ($dropdown && height) ? height : 0};
+    
 
     @media (max-width: 768px){
         position:sticky;
         box-shadow: none;
+        height:auto;
     }
     
 `
@@ -18,6 +23,8 @@ const StyledNavLink = styled(NavLink)`
     display: block;
     text-decoration: none;
     color:black;
+  
+
     &:hover{
         color: ${({ theme }) => theme.color.mainColor};
     }
@@ -33,9 +40,17 @@ const StyledNavLink = styled(NavLink)`
 `
 
 
-function Dropdown() {
+function Dropdown({ $dropdown }) {
+
+    const [height, setHeight] = useState("0px");
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        setHeight(`${dropdownRef.current.scrollHeight}px`);
+    }, [height]);
+
     return (
-        <DropdownContainer>
+        <DropdownContainer height={height} ref={dropdownRef} $dropdown={$dropdown}>
 
             <StyledNavLink to="/uslugi/monitoring">Monitoring</StyledNavLink>
             <StyledNavLink to="/uslugi/systemy-zabezpieczen">Systemy zabezpieczeń</StyledNavLink>
