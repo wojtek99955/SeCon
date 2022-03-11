f580005d9b7c9e5e930a7541365ff9e98c3e6461
Wojtek Książek <wojtek99955@gmail.com>
2022-03-11 12:10:42 +0100

    footer styles changed / links fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link, animateScroll as scroll } from 'react-scroll'
+import { animateScroll as scroll } from 'react-scroll'
 import logo from '../../assets/img/logo.jpeg'
-import { FooterSection, Container, Row, BottomBar, Logo } from './FooterStyles'
+import { FooterSection, Container, Nav, BottomBar, Logo, StyledHashLink, StyledNavLink, ContactButton, ContentWrapper } from './FooterStyles'
+
 
 
 function Footer() {
@@ -9,24 +10,33 @@ function Footer() {
     const scrollTop = () => {
         scroll.scrollToTop()
     }
+
     return (
         <FooterSection>
             <Container>
-                <Row>
-                    <ul>
-                        <li onClick={scrollTop}>home </li>
-                        <li><Link to="about">o nas</Link></li>
-                        <li><Link to="services">oferta</Link></li>
-                        <li><Link to="projects">realizacje</Link></li>
-                    </ul>
-                </Row>
-                <Row>
-                    <ul>
-                        <li>referencje</li>
-                        <li><Link to="contact">kontakt</Link></li>
-                    </ul>
-                </Row>
-                <Logo onClick={scrollTop} src={logo} />
+                <ContentWrapper>
+                    <StyledNavLink to="/"><Logo src={logo} /></StyledNavLink>
+                    <Nav>
+                        <h3 onClick={scrollTop}>Secon</h3>
+                        <ul>
+                            <li><StyledHashLink to="/#about">o nas</StyledHashLink></li>
+
+                            <li><StyledHashLink to="/#projects">realizacje</StyledHashLink></li>
+                            <li><StyledHashLink to="/#certificates">certyfikaty</StyledHashLink></li>
+                            <li><StyledHashLink to="/#contact">kontakt</StyledHashLink></li>
+                        </ul>
+                    </Nav>
+                    <Nav>
+                        <h3>Oferta</h3>
+                        <ul>
+                            <li><StyledNavLink to="/uslugi/monitoring">monitoring</StyledNavLink></li>
+                            <li><StyledNavLink to="/uslugi/monitoring">systemy zabezpieczeń</StyledNavLink></li>
+                            <li><StyledNavLink to="/uslugi/monitoring">kontrola dostępu</StyledNavLink></li>
+                            <li><StyledNavLink to="/uslugi/monitoring">pomiary instalacji elektrycznej</StyledNavLink></li>
+                        </ul>
+                    </Nav>
+                    <ContactButton to="/#contact">Kontakt</ContactButton>
+                </ContentWrapper>
             </Container>
             <BottomBar />
         </FooterSection>
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,40 +1,45 @@
 import styled from 'styled-components';
 import { device } from '../../assets/media'
+import { HashLink } from 'react-router-hash-link';
+import { NavLink } from 'react-router-dom'
 
 export const FooterSection = styled.footer``
 
 export const Container = styled.div`
     max-width: 1300px;
     margin: auto;
-    padding: 2rem 0;
+    padding: 2rem 1rem;
 `
 
-export const Row = styled.div`
-    display: flex;
-    justify-content: center;
+export const Nav = styled.nav`
 
-    &:nth-of-type(2) {
-        margin-top: 1.5rem;
-    }
     ul {
         list-style-type: none;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 2rem;
+
 
         @media ${device.tablet} {
             flex-direction: row;
         }
+
     }
     li {
-        font-size: 1rem;
+        font-size: ${({ theme }) => theme.fontSize.xs};
         font-weight: 500;
         cursor: pointer;
-        text-transform: uppercase;
+        text-align: center;
 
-        &:hover {
-            color: #f7941c;
+        @media ${device.laptop}{
+            text-align: start;
+        }
+    }
+    h3{
+        font-size: ${({ theme }) => theme.fontSize.s};
+        text-transform: uppercase;
+        margin-bottom:1rem;
+        text-align:center;
+
+        @media ${device.laptop}{
+            text-align: start;
         }
     }
 `
@@ -45,9 +50,50 @@ export const BottomBar = styled.div`
 `
 
 export const Logo = styled.img`
-width:6rem;
-display: block;
-margin: auto;
-padding-top: 2rem;
-cursor: pointer;
+    width:6rem;
+    display: block;
+    cursor: pointer;
+    margin:auto;
+`
+export const StyledHashLink = styled(HashLink)`
+    text-decoration:none;
+    color: black;
+    display: block;
+    padding: 0.2rem 0;
+
+
+    &:hover {
+        color: #f7941c;
+    }
+`
+export const StyledNavLink = styled(NavLink)`
+    text-decoration:none;
+    color: black;
+    display: block;
+    padding: 0.2rem 0;
+
+    &:hover {
+        color: #f7941c;
+    }
+`
+export const ContactButton = styled(HashLink)`
+    background-color: ${({ theme }) => theme.color.mainColor};
+    border: none;
+    padding: 0.6rem 2rem;
+    align-self: center;
+    color:white;
+    border-radius: 10px;
+    cursor: pointer;
+    text-decoration: none;
+`
+export const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    @media ${device.laptop}{
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
 `
