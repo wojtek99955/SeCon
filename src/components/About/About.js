55fefe888564666c64a5024f90d71270a4c10db2
Wojtek Książek <wojtek99955@gmail.com>
2022-03-19 09:53:35 +0100

    About AOS added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { Section, Container, StyledImg, AboutInfo } from './AboutStyles'
-
-
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 function About() {
+
+    useEffect(() => {
+        Aos.init({ duration: 1000 })
+    }, []);
+
     return (
         <Section id="about">
             <Container>
-                <StyledImg />
-                <AboutInfo>
+                <StyledImg data-aos="fade-right" />
+                <AboutInfo data-aos="fade-left">
                     <h1>O nas</h1>
                     <h2>
                         Ponad 30 lat doświadczenia
                     </h2>
                     <p>
-                        Firma powstała w 1991 roku  jako prywatny Zakład Usług Elektrotechnicznych, przekształacając się z czasem w zakład SECON Technicznea Ochrona Mienia. Oprócz obowiązkowych uprawnień wynikających ze stosownych przepisów dotyczących tego rodzaju działalności, firma posoada również Koncesję MSWiA.
+                        Firma powstała w 1991 roku
+                        jako prywatny Zakład Usług Elektrotechnicznych,
+                        przekształacając się z czasem w zakład SECON
+                        Technicznea Ochrona Mienia. Oprócz obowiązkowych
+                        uprawnień wynikających ze stosownych przepisów
+                        dotyczących tego rodzaju działalności, firma
+                        posoada również Koncesję MSWiA.
                     </p>
                     <p>
-                        Obecnie na bazie zdobytego doświadczenia, wiedzy i posiadanych uprawnień, zajmujemy się zarówno małymi, dla indywidualnych odbiorców prostymi systemami alarmowymi, telewizji dozorowej, kontroli dostępu i rejestracji czasu pracy, oraz dużymi, które wymagają kompleksowego opracowania i integracji. Z oczywistych względów nie podajemy listy referencyjnej jednak po dokonaniu uzgodnień, służymy odpowiednimi informacjami.
+                        Obecnie na bazie zdobytego doświadczenia,
+                        wiedzy i posiadanych uprawnień, zajmujemy
+                        się zarówno małymi, dla indywidualnych
+                        odbiorców prostymi systemami alarmowymi,
+                        telewizji dozorowej, kontroli dostępu i
+                        rejestracji czasu pracy, oraz dużymi,
+                        które wymagają kompleksowego opracowania i
+                        integracji. Z oczywistych względów nie
+                        podajemy listy referencyjnej jednak po
+                        dokonaniu uzgodnień, służymy odpowiednimi
+                        informacjami.
                     </p>
-                    <p>Dbając o jakość wykonywanych przez firmę systemów przyjęliśmy za standard 36 miesięczną gwarancję w zakresie wykonywanych prac.</p>
+                    <p>Dbając o jakość wykonywanych przez firmę
+                        systemów przyjęliśmy za standard 36
+                        miesięczną gwarancję w zakresie wykonywanych
+                        prac.
+                    </p>
                 </AboutInfo>
             </Container>
         </Section>
