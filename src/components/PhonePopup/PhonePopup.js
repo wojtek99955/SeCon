4fe2d4c84af6a6c0237ec4ac2fb9fe13d14f12a0
Wojtek Książek <wojtek99955@gmail.com>
2022-04-02 11:13:12 +0200

    PhonePopup styles update

--- src/components/PhonePopup/PhonePopup.js
+++ src/components/PhonePopup/PhonePopup.js
@@ -4,11 +4,19 @@ import {Phone} from '@styled-icons/boxicons-solid/Phone'
 const PhoneIcon = styled(Phone)`
     width:3rem;
     position: absolute;
+    color: white;
 `
 
 const Container = styled.div`
     position: fixed;
     top: 90%;
+    left: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2.5rem;
+    border-radius: 50%;
+    background-color: ${({theme}) => theme.color.mainColor};
 `
 
 function PhonePopup() {
