f8d36530a801668b31d18951eab86cfd3d3f88bc
Wojtek Książek <wojtek99955@gmail.com>
2022-04-16 10:16:19 +0200

    NoMatch route styles update

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.section`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
 function NoMatch() {
-    return <div>Page not found</div>
+    return (
+        <Container>
+            <div>Page not found</div>
+        </Container>
+    )
 }
 
 export default NoMatch
