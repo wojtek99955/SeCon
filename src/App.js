7b7feafd7eeb9ced9430fd908d5315b5af202e60
Wojtek Książek <wojtek99955@gmail.com>
2022-04-16 10:12:06 +0200

    NoMatch route component added

--- src/App.js
+++ src/App.js
@@ -10,28 +10,37 @@ import { Routes, Route } from 'react-router-dom'
 import Alarms from './components//subpages/Alarms/Alarms'
 import AccessControl from './components/subpages/AccessControl/AccessControl'
 import Measurement from './components/subpages/Measurement/Measurement'
-import { useEffect } from 'react';
-import PhonePopup from './components/PhonePopup/PhonePopup';
-
+import { useEffect } from 'react'
+import PhonePopup from './components/PhonePopup/PhonePopup'
+import NoMatch from './components/NoMatch/NoMatch'
 
 function App() {
-
     useEffect(() => {
-        document.title = "SECON - techniczna ochrona mienia"
-    }, []);
+        document.title = 'SECON - techniczna ochrona mienia'
+    }, [])
     return (
         <div>
             <GlobalStyles />
             <ThemeProvider theme={theme}>
                 <ScrollTopButton />
-                <PhonePopup/>
+                <PhonePopup />
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/uslugi/monitoring" element={<Monitoring />} />
-                    <Route path="/uslugi/systemy-zabezpieczen" element={<Alarms />} />
-                    <Route path="/uslugi/kontrola-dostepu-rejestracja-czasu-pracy" element={<AccessControl />} />
-                    <Route path="/uslugi/pomiary-instalacji-i-urzadzen-elektrycznych" element={<Measurement />} />
+                    <Route
+                        path="/uslugi/systemy-zabezpieczen"
+                        element={<Alarms />}
+                    />
+                    <Route
+                        path="/uslugi/kontrola-dostepu-rejestracja-czasu-pracy"
+                        element={<AccessControl />}
+                    />
+                    <Route
+                        path="/uslugi/pomiary-instalacji-i-urzadzen-elektrycznych"
+                        element={<Measurement />}
+                    />
+                    <Route path="*" element={<NoMatch />} />
                 </Routes>
                 <Footer />
             </ThemeProvider>
---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function NoMatch() {
+    return <div>Page not found</div>
+}
+
+export default NoMatch
